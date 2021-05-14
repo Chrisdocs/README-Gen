cc517270d0071102d872fa23e9dfa0ba46dd8f1c
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-05-14 17:39:14 -0500

    inital inquirer questions formatting

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Please enter a title for your project. (required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a description for your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
+        }
+
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +42,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
